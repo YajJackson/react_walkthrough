3316057fe7de30b0635aadd21bd7e6b0ed107524
Jay Jackson <jaynickjackson@gmail.com>
2017-10-02 11:43:12 -0400

    Introducing JSX

--- src/index.js
+++ src/index.js
@@ -6,16 +6,18 @@ const formatName = (user) => {
   return user.firstName + ' ' + user.lastName
 }
 
+const getGreeting =(user) => {
+  if (user) {
+    return <h1>Hello, {formatName(user)}!</h1>
+  }
+  return <h1>Hello, Stranger.</h1>
+}
 const user = {
   firstName: 'Jay',
   lastName: 'Jackson'
 }
 
-const element = (
-  <h1>
-    Hello, {formatName(user)}
-  </h1>
-)
+const element = <div>{getGreeting(user)}</div>
 
 ReactDOM.render(
   element,
