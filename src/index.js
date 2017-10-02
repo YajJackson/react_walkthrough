88a7535d1df4484fc8c9e28f15cf2ada14afff76
Jay Jackson <jaynickjackson@gmail.com>
2017-10-02 11:54:24 -0400

    Rendering Elements: Tick example

--- src/index.js
+++ src/index.js
@@ -1,43 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.css'
 
-const formatName = (user) => {
-  return user.firstName + ' ' + user.lastName
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  )
+
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  )
 }
 
-const getGreeting =(user) => {
-  if (user) {
-    return <h1>Hello, {formatName(user)}!</h1>
-  }
-  return <h1>Hello, Stranger.</h1>
-}
-const user = {
-  firstName: 'Jay',
-  lastName: 'Jackson'
-}
-
-// JSX tags may contain children
-const element = (
-  <div>
-    {getGreeting(user)}
-  </div>
-)
-
-// Babel compiles JSX down to React.createElement() calls.
-// const element = (
-//   <h1 className='greeting'>
-//     Hello, world!
-//   </h1>
-// )
-
-// const element = React.createElement(
-//   'h1',
-//   {className: 'greeting'},
-//   'hello, world!'
-// )
-
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-)
+setInterval(tick, 1000)
