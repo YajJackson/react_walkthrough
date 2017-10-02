a8c4a08553064d9a3776aebf38ac7844afca7696
Jay Jackson <jaynickjackson@gmail.com>
2017-10-02 13:38:46 -0400

    State and Lifcycle hooks, Clock example

--- src/index.js
+++ src/index.js
@@ -2,28 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 
-// The following are all equivalent from React's point of view (slight difference with the arrow function)
-// function Welcome1(props) {
-//   return <h1>Hello, {props.name}</h1>
-// }
+class Clock extends React.Component {
+  // Add a class constructor that assigns the initial this.state
+  constructor(props) {
+    super(props)
+    this.state = {date: new Date()}
+  }
 
-// const Welcome2 = (props) => {
-//   return <h1>Hello, {props.name} </h1>
-// }
+  // Lifecycle hooks
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    )
+  }
+  // if the Click component is ever removed from the DOM, the timer is stopped.
+  componentWillUnmount() {
+    clearInterval(this.timerID)
+  }
 
-// class Welcome3 extends React.Component {
-//   render() {
-//     return <h1>Hello, {this.props.name}</h1>
-//   }
-// }
+  // Implement tick() method that runs every second
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
 
-const element = <Welcome name='Jay' />
-
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(
-  element,
+  <Clock />,
   document.getElementById('root')
 )
